bf87522c556d7c55474ba8b6b28136a7adf71bac
lakshmi@tw21 <rp.lakshmi.45@gmail.com>
2012-09-24 10:26:46 +0530

    changes made to the repository

--- src/com/twu28/biblioteca/Mainprogram.java
+++ src/com/twu28/biblioteca/Mainprogram.java
@@ -6,15 +6,12 @@ public class Mainprogram {
 
     inputoutputinterface inputoutputinterface = new userinputstub();
     Display display = new Display();
-    Book book1 = new Book(1,"First Book");
-    Book book2 = new Book(2,"Second Book");
-    Book book3 = new Book(3,"Third Book");
     BookManager bookManager = new BookManager();
 
     public void main(String[] args) {
-        bookManager.addnew(book1);
-        bookManager.addnew(book2);
-        bookManager.addnew(book3);
+        bookManager.addnew(new Book(1,"First Book"));
+        bookManager.addnew(new Book(2,"Second Book"));
+        bookManager.addnew(new Book(3,"Third Book"));
 
         display.print_welcome();
         display.print_menu();
@@ -30,22 +27,29 @@ public class Mainprogram {
         }
 
         else if(choice==2){
-            display.display_of_books(bookManager);
-            display.reserve_book_first_line();
-            String book_to_reserved = inputoutputinterface.readLine();
-            Book book = new Book(book_to_reserved);
-            if(display.reserve_book(book,bookManager))
-                inputoutputinterface.println("Thank You!Enjoy The Book");
-            else
-                inputoutputinterface.println("Sorry!We Dont Have That Book Yet");
+                display.display_of_books(bookManager);
+                display.reserve_book_first_line();
+                String book_to_reserved = inputoutputinterface.readLine();
+                Book book = new Book(book_to_reserved);
+                if(display.reserve_book(book,bookManager))
+                    inputoutputinterface.println("Thank You!Enjoy The Book");
+                else
+                    inputoutputinterface.println("Sorry!We Dont Have That Book Yet");
+
+
         }
 
         else if(choice==3){
-            display.check_library_number();
+
+                display.check_library_number();
+
+
         }
         else
             exit(0);
     }
+
+
 
     public void setInputoutputinterface(inputoutputinterface inputoutputinterface){
         this.inputoutputinterface = inputoutputinterface;
